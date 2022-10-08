a9b283372ec3b1681a1ed8b83f4a062b3031ba8b
DavidXP1225 <62429061+DavidXP1225@users.noreply.github.com>
2022-10-08 11:03:49 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-/* 
-
-I built this login form to block the front end of most of my freelance wordpress projects during the development stage. 
-
-This is just the HTML / CSS of it but it uses wordpress's login system. 
-
-Nice and Simple
-
-*/
 window.location.assign(‘https://www.sportpresszo.hu/’);
